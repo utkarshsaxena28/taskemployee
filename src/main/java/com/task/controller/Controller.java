10d3a0ae288360ea278a911ef8c78aca89101193
Utkarsh Saxena <utkarshsaxena.saxena28@gmail.com>
2022-07-28 16:15:37 +0530

    Adding deletemapping
    Some Description

--- src/main/java/com/task/controller/Controller.java
+++ src/main/java/com/task/controller/Controller.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.task.entity.Employee;
 import com.task.service.EmployeeService;
@@ -28,7 +32,7 @@ public class Controller
 	    service.save(empy);
 	 }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/employee/{id}")
     public void delete(@PathVariable Integer id) {
         service.delete(id);
     }
